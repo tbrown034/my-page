b2d81d6c25c03f2986ede5637592c3b6d9d1a3de
tbrown034 <tbrown034@gmail.com>
2022-10-15 02:08:15 -0500

    h1 to h6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>Hello World</p>
-      
+      <div id="H1_H6">
+        <h1>Hello World.</h1>
+        <h2>This is my website. I created it.</h2>
+        <h3>This is an H3 tag, just for reference.</h3>
+        <h4>Do H4 tags exisit? Guess so!</h4>
+        <h5>How about five?</h5>
+        <h6>6 is it.</h6>
+      </div>
     </div>
   );
 }
